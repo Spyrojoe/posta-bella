d944ab99d5dee70b67c215cecc331cdd14ae2e2f
Brad Brock <brad.brock1@gmail.com>
2017-07-01 12:48:02 -0500

    Initial commit, markup and style from prototype.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('task-name', function() {
+  // Stuff here
+});
+
+gulp.task('hello', function() {
+  console.log('Hello Zell');
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('app/scss/*.scss', ['sass']);
+});
